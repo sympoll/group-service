61a87d41b0f81e1558d99342eaf7e4897b25206a
Ronenii <ronen.glm@gmail.com>
2024-04-23 00:48:45 +0300

    -Initialized repo

---
+++ src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/GroupManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.MTAPizza.Sympoll.groupmanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MTAPizza/Sympoll/groupmanagementservice/GroupManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MTAPizza.Sympoll.groupmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
