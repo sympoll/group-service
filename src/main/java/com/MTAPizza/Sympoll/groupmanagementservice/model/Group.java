87d5f42005a747678d318dfeed988e23e780bf42
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-08-11 16:33:44 +0300

    Added classes.

---
+++ src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/model/Group.java
@@ -0,0 +1,11 @@
+package com.MTAPizza.Sympoll.groupmanagementservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "groups")
+@Data
+public class Group {
+}
