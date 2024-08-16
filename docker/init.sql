71c2dce94211db91730bda5a65777840629915ca
Ronenii <ronen.glm@gmail.com>
2024-08-16 13:53:18 +0300

    - Added data files to git ignore - Group names in init sql match ini sql in deployment playground

--- docker/init.sql
+++ docker/init.sql
@@ -17,17 +17,17 @@ CREATE TABLE members
 
 -- Insert sample data into the groups table
 INSERT INTO groups (group_id, group_name, description, creator_id)
-VALUES ('group1', 'Developers', 'Group for developers to discuss and share knowledge.',
+VALUES ('group_1', 'Developers', 'Group for developers to discuss and share knowledge.',
         'a1e8a3d2-07a8-11ec-bf63-0242ac130002'),
-       ('group2', 'Designers', 'Group for designers to collaborate on projects.',
+       ('group_2', 'Designers', 'Group for designers to collaborate on projects.',
         'a1e8a3d2-07a8-11ec-bf63-0242ac130003'),
-       ('group3', 'Managers', 'Group for managers to discuss strategies and plans.',
+       ('group_3', 'Managers', 'Group for managers to discuss strategies and plans.',
         'a1e8a3d2-07a8-11ec-bf63-0242ac130004');
 
 -- Insert sample data into the members table
 INSERT INTO members (user_id, group_id)
-VALUES ('a1e8a3d2-07a8-11ec-bf63-0242ac130005', 'group1'),
-       ('a1e8a3d2-07a8-11ec-bf63-0242ac130006', 'group1'),
-       ('a1e8a3d2-07a8-11ec-bf63-0242ac130007', 'group2'),
-       ('a1e8a3d2-07a8-11ec-bf63-0242ac130008', 'group2'),
-       ('a1e8a3d2-07a8-11ec-bf63-0242ac130009', 'group3');
+VALUES ('a1e8a3d2-07a8-11ec-bf63-0242ac130005', 'group_1'),
+       ('a1e8a3d2-07a8-11ec-bf63-0242ac130006', 'group_1'),
+       ('a1e8a3d2-07a8-11ec-bf63-0242ac130007', 'group_2'),
+       ('a1e8a3d2-07a8-11ec-bf63-0242ac130008', 'group_2'),
+       ('a1e8a3d2-07a8-11ec-bf63-0242ac130009', 'group_3');
