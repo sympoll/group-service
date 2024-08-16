4d9f8d2781d3eefcfe0a9f6f8562b5b52604ecaf
Ronenii <ronen.glm@gmail.com>
2024-08-16 12:35:17 +0300

    - Changed data annotations - added test image to docker compose - Redefined db url in application.properties

--- src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/model/member/id/MemberId.java
+++ src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/model/member/id/MemberId.java
@@ -3,40 +3,19 @@ package com.MTAPizza.Sympoll.groupmanagementservice.model.member.id;
 
 import com.MTAPizza.Sympoll.groupmanagementservice.model.member.Member;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Objects;
 import java.util.UUID;
 
+@Data
 public class MemberId implements Serializable {
 
     private String groupId;
 
     private UUID userId;
-
-    public MemberId() {
-    }
-
-    public MemberId(UUID userId, String groupId) {
-        this.userId = userId;
-        this.groupId = groupId;
-    }
-
-    public String getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(String groupId) {
-        this.groupId = groupId;
-    }
-
-    public UUID getUserId() {
-        return userId;
-    }
-
-    public void setUserId(UUID userId) {
-        this.userId = userId;
-    }
 
     @Override
     public boolean equals(Object o) {
