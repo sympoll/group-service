bd446711d7d74bbddc2b4acaabd74d77b064b39b
Roy <roytol12@gmail.com>
2024-08-20 15:22:40 +0300

    Created endpoints for the user role service methods in the service controller

--- src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/repository/RoleRepository.java
+++ src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package com.MTAPizza.Sympoll.groupmanagementservice.repository;
 
 import com.MTAPizza.Sympoll.groupmanagementservice.model.role.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 
 }
--- src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/repository/UserRoleRepository.java
+++ src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/repository/UserRoleRepository.java
@@ -3,9 +3,11 @@ package com.MTAPizza.Sympoll.groupmanagementservice.repository;
 import com.MTAPizza.Sympoll.groupmanagementservice.model.user.role.UserRole;
 import com.MTAPizza.Sympoll.groupmanagementservice.model.user.role.id.UserRoleId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, UserRoleId> {
     UserRole findByUserIdAndGroupId(UUID userId, String groupId);
 }
