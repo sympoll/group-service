cc29a2055de13f02716ad9700adb5ed2eed9b9c5
Roy <roytol12@gmail.com>
2024-08-26 18:15:48 +0300

    Modified get all members method to retrieve: user id, username and the role name of the group member

--- src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/model/role/RoleName.java
+++ src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/model/role/RoleName.java
@@ -8,7 +8,8 @@ public enum RoleName {
 
     @Override
     public String toString() {
-        return this.name().toLowerCase();
+        String name = this.name().toLowerCase();
+        return name.substring(0, 1).toUpperCase() + name.substring(1);
     }
 }
 
