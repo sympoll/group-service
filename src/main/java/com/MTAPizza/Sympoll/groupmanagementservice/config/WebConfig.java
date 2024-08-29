edb34a3b945ebc748b511cea64cab79ebc5031a1
Ronenii <ronen.glm@gmail.com>
2024-08-29 13:30:46 +0300

    Added origin

--- src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/config/WebConfig.java
+++ src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/config/WebConfig.java
@@ -1,15 +1,20 @@
 package com.MTAPizza.Sympoll.groupmanagementservice.config;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private static final Logger logger = LoggerFactory.getLogger(WebConfig.class);
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        logger.info("Configuring CORS settings");
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")  // Replace '*' with the specific origin
+                .allowedOrigins("http://localhost:8080", "http://localhost")  // Replace '*' with the specific origin
                 .allowCredentials(true)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
