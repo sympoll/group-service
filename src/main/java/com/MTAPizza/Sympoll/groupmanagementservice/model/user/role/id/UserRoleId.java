d1150fa0cacac955b4786b8b8a1b903d63ee46fb
Roy <roytol12@gmail.com>
2024-08-31 17:46:40 +0300

    role name is no longer a key in user roles

--- src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/model/user/role/id/UserRoleId.java
+++ src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/model/user/role/id/UserRoleId.java
@@ -11,18 +11,16 @@ public class UserRoleId implements Serializable {
 
     private UUID userId;
 
-    private String roleName;
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         UserRoleId that = (UserRoleId) o;
-        return Objects.equals(groupId, that.groupId) && Objects.equals(userId, that.userId) && Objects.equals(roleName, that.roleName);
+        return Objects.equals(groupId, that.groupId) && Objects.equals(userId, that.userId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(groupId, userId, roleName);
+        return Objects.hash(groupId, userId);
     }
 }
