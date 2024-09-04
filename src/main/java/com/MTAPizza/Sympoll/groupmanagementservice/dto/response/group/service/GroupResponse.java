da696a641c049d96fd834d51095eac6bfacd4708
Idan <104992892+Idan-sh@users.noreply.github.com>
2024-09-04 11:28:58 +0300

    Added pattern to time created to correctly return the time including the timezone.

--- src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/dto/response/group/service/GroupResponse.java
+++ src/main/java/com/MTAPizza/Sympoll/groupmanagementservice/dto/response/group/service/GroupResponse.java
@@ -1,5 +1,7 @@
 package com.MTAPizza.Sympoll.groupmanagementservice.dto.response.group.service;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -9,6 +11,7 @@ public record GroupResponse (
     String groupName,
     String description,
     UUID creatorId,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     LocalDateTime timeCreated,
     List<MemberResponse> membersList
 ) {}
